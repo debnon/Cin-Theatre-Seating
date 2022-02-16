d846a04fae17b676b5b725efe4f77a16be628c80
Ben Donaghy <bendonaghy321@gmail.com>
2022-02-16 20:21:12 +0000

    initializing theatre framework

---
+++ src/main/java/customer.java
@@ -0,0 +1,14 @@
+// rows A - C, columns (seats) 1 - 5
+
+// customer requests 1 - 3 tickets corresponding to particular seats for a movie
+// customer is allocated appropriate seats from the seating plan
+// seats recorded as allocated
+
+public class customer {
+
+    customer
+
+    newOrder(String movieName) {
+        
+    }
+}
---
+++ src/test/java/theatreTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class theatreTest {
+
+    @Test
+    public void seatingGridInit() {
+        int actualNumber = 5;
+        Assertions.assertEquals(5, actualNumber);
+    }
+
+}
