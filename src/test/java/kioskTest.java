752088a9f4faeae10b8f454dce901c8623c2b81f
Ben Donaghy <bendonaghy321@gmail.com>
2022-03-17 21:13:30 +0000

    added customer ID tests to kioskTest

--- src/test/java/kioskTest.java
+++ src/test/java/kioskTest.java
@@ -1,18 +1,28 @@
+import org.junit.Before;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class kioskTest {
 
-    @Test
-    public void kioskInitTest() throws Exception {
-        Kiosk kiosk = new Kiosk();
-        Customer customer = new Customer("James", kiosk);
+    Customer customer;
+    Kiosk kiosk;
+
+
+    @BeforeEach
+    public void kioskInit() {
+        kiosk = new Kiosk();
+        customer = new Customer("James", kiosk);
         Theatre theatre1 = new Theatre(kiosk);
         theatre1.addMovie("Wallie");
+    }
 
+    @Test
+    public void kioskCustomerIdTest() throws Exception {
         customer.orderTicket("Wallie");
-        String customerID = customer.getCustomerID();
-
         Assertions.assertEquals(customer.getCustomerID(), kiosk.getAllCustomerIDs().get(0));
+
+        Customer customer2 = new Customer("Alice", kiosk);
+        Assertions.assertEquals(customer2.getCustomerID(), kiosk.getAllCustomerIDs().get(1));
     }
 }
