ec32db7989fe5bfd5819e7f8f6da4490d56ddce2
Ben Donaghy <bendonaghy321@gmail.com>
2022-03-17 20:30:32 +0000

    expanded theatre tests to include checking for movie or lack of movie presence in a theatre

--- src/test/java/theatreTest.java
+++ src/test/java/theatreTest.java
@@ -1,12 +1,34 @@
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class theatreTest {
 
+    Theatre theatre;
+
+    @BeforeEach
+    public void theatreInit() {
+        Kiosk kiosk = new Kiosk();
+        Customer customer = new Customer("Test", kiosk);
+        theatre = new Theatre(kiosk);
+        theatre.addMovie("Test Movie");
+        theatre.addMovie("wallie");
+    }
+
     @Test
-    public void seatingGridInit() {
-        int actualNumber = 5;
-        Assertions.assertEquals(5, actualNumber);
+    public void checkMovieInTheatre() {
+
+        Assertions.assertEquals(true, theatre.checkMoviePresence("Test Movie"));
+        Assertions.assertEquals(true, theatre.checkMoviePresence("wallie"));
+    }
+
+    @Test
+    public void checkMovieNotInTheatre() {
+
+        Assertions.assertEquals(false, theatre.checkMoviePresence("Not Test Movie"));
+        Assertions.assertEquals(false, theatre.checkMoviePresence(""));
+        Assertions.assertEquals(false, theatre.checkMoviePresence("test Movie"));
+        Assertions.assertEquals(false, theatre.checkMoviePresence("Wallie"));
     }
 
 }
